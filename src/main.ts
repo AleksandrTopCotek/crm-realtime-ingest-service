32526b6f6c417215c672e329fb3a679de43477d2
AleksandrTopCotek <aleksandr.i@topcotek.com>
2025-12-22 15:00:13 +0200

    feat: implement config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = Number(configService.get('PORT') ?? 3000);
+  await app.listen(port);
+  logger.log(`Ingest service is started at ${port}`);
 }
 bootstrap();
