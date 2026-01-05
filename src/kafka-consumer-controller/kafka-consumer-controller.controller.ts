25a2147726bd41c13f400795413305efec6954a6
AleksandrTopCotek <aleksandr.i@topcotek.com>
2026-01-05 17:10:57 +0200

    feat: update kafka env links

--- src/kafka-consumer-controller/kafka-consumer-controller.controller.ts
+++ src/kafka-consumer-controller/kafka-consumer-controller.controller.ts
@@ -3,10 +3,9 @@ import { MessagePattern, Payload, Ctx, KafkaContext } from '@nestjs/microservice
 import { HandleConfigService } from 'src/shared/services/handle-config-service/handle-config-service.service';
 import { SchemaRegistryService } from 'src/shared/services/schema-registry/schema-registry.service';
 
-const TOPIC_SPORT_ROUND = process.env.KF_SPORT_ROUND_TOPIC_NAME ?? 'vegasnova_hrzn05_prod_tagged_sport_round_data';
-const TOPIC_PROFILE_VERIFICATION_STATE =
-  process.env.KF_PROFILE_VERIFICATION_TOPIC_NAME ?? 'vegasnova_hrzn05_prod_profile_verification_state';
-const TOPIC_BONUS_GAME = process.env.KF_BONUS_GAME_TOPIC_NAME ?? 'vegasnova_hrzn05_prod_converted_bonus_game';
+const TOPIC_SPORT_ROUND = process.env.KF_SPORT_ROUND_TOPIC_NAME ?? '';
+const TOPIC_PROFILE_VERIFICATION_STATE = process.env.KF_PROFILE_VERIFICATION_TOPIC_NAME ?? '';
+const TOPIC_BONUS_GAME = process.env.KF_BONUS_GAME_TOPIC_NAME ?? '';
 
 @Controller()
 export class KafkaConsumerController {
