bb159ecdbb87786367623be3e59d13e4a5853a3d
AleksandrTopCotek <aleksandr.i@topcotek.com>
2026-01-05 14:37:54 +0200

    feat:add extra configs

--- src/deposit/deposit.controller.spec.ts
+++ src/deposit/deposit.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DepositController } from './deposit.controller';
 import { DepositService } from './deposit.service';
 import { SchemaService } from 'src/shared/services/schema/schema.service';
+import { SchemaRegistryService } from 'src/shared/services/schema-registry/schema-registry.service';
 
 describe('DepositController', () => {
   let controller: DepositController;
@@ -15,6 +16,12 @@ describe('DepositController', () => {
           provide: SchemaService,
           useValue: {
             getSchema: jest.fn(),
+          },
+        },
+        {
+          provide: SchemaRegistryService,
+          useValue: {
+            decodeConfluentAvro: jest.fn(),
           },
         },
       ],
--- src/deposit/deposit.module.ts
+++ src/deposit/deposit.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { DepositService } from './deposit.service';
 import { DepositController } from './deposit.controller';
 import { SchemaService } from 'src/shared/services/schema/schema.service';
+import { SchemaRegistryModule } from 'src/shared/services/schema-registry/schema-registry.module';
 
 @Module({
+  imports: [SchemaRegistryModule],
   controllers: [DepositController],
   providers: [DepositService, SchemaService],
 })
