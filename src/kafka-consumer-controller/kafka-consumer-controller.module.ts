9c2f43e584abc627c15d22d803346538546e1f71
AleksandrTopCotek <aleksandr.i@topcotek.com>
2026-01-05 14:19:46 +0200

    feat:add scheme registry decoder

--- src/kafka-consumer-controller/kafka-consumer-controller.module.ts
+++ src/kafka-consumer-controller/kafka-consumer-controller.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { KafkaConsumerController } from './kafka-consumer-controller.controller';
+import { SchemaRegistryModule } from 'src/shared/services/schema-registry/schema-registry.module';
 
 @Module({
+  imports: [SchemaRegistryModule],
   controllers: [KafkaConsumerController],
 })
 export class KafkaConsumerControllerModule {}
---
+++ src/shared/services/schema-registry/schema-registry.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { SchemaRegistryService } from './schema-registry.service';
+
+@Module({
+  providers: [SchemaRegistryService],
+  exports: [SchemaRegistryService],
+})
+export class SchemaRegistryModule {}
+
+
